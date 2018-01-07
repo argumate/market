ecaa56018332493c2e256283f72309e5fc0ff7f5
Argumate <argumate@argumate.com>
2018-01-07 16:18:13 +1100

    HTTP server module that accepts JSON queries.

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ extern crate rusqlite;
 
 #[macro_use]
 extern crate serde_derive;
-
 extern crate serde;
 extern crate serde_json;
 
 extern crate uuid;
 
+extern crate hyper;
+extern crate futures;
+
 pub mod db;
 pub mod market;
+pub mod server;
 
 use std::env;
 use std::collections::HashMap;
@@ -23,71 +26,264 @@ use db::DB;
 use market::Market;
 use market::types::{ID, Dollars, ArgList, User, IOU, Transfer, Cond, Offer, OfferUpdate, Entity, Rel, Pred, Depend};
 use market::msgs::{Request, Response, Query, Item, ItemUpdate};
+use server::run_server;
 
-enum CmdLine {
-    Help,
-    Command(Command),
+struct CmdLine {
+    config: Config,
+    command: Command
+}
+
+struct Config {
+    help: bool,
+    db_filename: String
 }
 
 #[derive(Debug)]
 enum Command {
-    Init(String),
-    Status(String),
+    Usage,
+    Init,
+    Status,
+    Server(String)
 }
 
 fn parse_command_line(opts: &Options, args: &Vec<String>) -> Result<CmdLine, Error> {
     let matches = opts.parse(&args[1..])?;
 
-    if matches.opt_present("h") {
-        return Ok(CmdLine::Help);
-    }
-    let file = match matches.opt_str("f") {
+    let help = matches.opt_present("h");
+    let db_filename = match matches.opt_str("f") {
         None => {
             return Err(failure::err_msg("missing --file argument"));
         }
         Some(f) => f
     };
-    let cmd = if !matches.free.is_empty() {
-        matches.free[0].clone()
+    let config = Config { help, db_filename };
+    let command = if !matches.free.is_empty() {
+        match matches.free[0].as_ref() {
+            "init" => Command::Init,
+            "status" => Command::Status,
+            "server" => Command::Server(String::from("127.0.0.1:8000")),
+            _ => return Err(failure::err_msg("unknown command")),
+        }
     } else {
-        return Err(failure::err_msg("missing command"));
+        Command::Usage
     };
-    match cmd.as_ref() {
-        "init" => Ok(CmdLine::Command(Command::Init(file))),
-        "status" => Ok(CmdLine::Command(Command::Status(file))),
-        _ => Err(failure::err_msg("unknown command"))
-    }
+    Ok(CmdLine { config, command })
 }
 
-fn print_usage(program: &str, opts: Options) {
+fn print_usage(program: &str, opts: &Options) {
     let brief = format!("Usage: {} [options] CMD", program);
     print!("{}", opts.usage(&brief));
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let program = &args[0];
     
     let mut opts = Options::new();
-    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("h", "help", "print help");
     opts.optopt("f", "file", "set database filename", "FILE");
 
-    match parse_command_line(&opts, &args) {
-        Ok(c) => {
-            match c {
-                CmdLine::Help => print_usage(&program, opts),
-                CmdLine::Command(cmd) => {
-                    match do_command(cmd) {
-                        Ok(()) => { }
-                        Err(e) => println!("{}", e)
-                    }
-                }
-            }
-        }
+    match main2(&opts, &args) {
+        Ok(()) => { },
         Err(e) => {
             println!("{}", e);
         }
-    };
+    }
+}
+
+fn main2(opts: &Options, args: &Vec<String>) -> Result<(), Error> {
+    let cmdline = parse_command_line(&opts, &args)?;
+    if cmdline.config.help {
+        // FIXME
+    }
+    match cmdline.command {
+        Command::Usage => {
+            let program = &args[0];
+            print_usage(&program, &opts);
+            Ok(())
+        },
+        Command::Init => init(&cmdline.config),
+        Command::Status => status(&cmdline.config),
+        Command::Server(addr) => server(&cmdline.config, &addr)
+    }
+}
+
+fn server(config: &Config, addr: &str) -> Result<(), Error> {
+    let db = DB::open_read_write(&config.db_filename)?;
+    let market = Market::open_existing(db)?;
+    run_server(market, addr)
+}
+
+fn init(config: &Config) -> Result<(), Error> {
+    let db = DB::open_read_write(&config.db_filename)?;
+    let mut market = Market::create_new(db)?;
+
+    let mrfoo = market.do_request(Request::Create(
+        Item::User(User {
+            user_name: String::from("Mr Foo"),
+            user_locked: false
+        })))?.unwrap_id();
+
+    let mrbar = market.do_request(Request::Create(
+        Item::User(User {
+            user_name: String::from("Mr Bar"),
+            user_locked: false
+        })))?.unwrap_id();
+
+    let trump = market.do_request(Request::Create(
+        Item::Entity(Entity {
+            entity_name: String::from("Donald Trump"),
+            entity_type: String::from("person"),
+        })))?.unwrap_id();
+
+    let jeb = market.do_request(Request::Create(
+        Item::Entity(Entity {
+            entity_name: String::from("Jeb Bush"),
+            entity_type: String::from("person"),
+        })))?.unwrap_id();
+
+    let repub = market.do_request(Request::Create(
+        Item::Entity(Entity {
+            entity_name: String::from("Republican Party"),
+            entity_type: String::from("party"),
+        })))?.unwrap_id();
+
+    let _dem = market.do_request(Request::Create(
+        Item::Entity(Entity {
+            entity_name: String::from("Democratic Party"),
+            entity_type: String::from("party"),
+        })))?.unwrap_id();
+
+    market.do_request(Request::Create(
+        Item::Rel(Rel {
+            rel_type: String::from("party"),
+            rel_from: jeb,
+            rel_to: repub.clone(),
+        })))?;
+
+    market.do_request(Request::Create(
+        Item::Rel(Rel {
+            rel_type: String::from("party"),
+            rel_from: trump.clone(),
+            rel_to: repub,
+        })))?;
+
+    let nominee2020 = market.do_request(Request::Create(
+        Item::Pred(Pred {
+            pred_name: String::from("Party nominee for 2020 election"),
+            pred_args: ArgList::from("party,person"),
+            pred_value: None
+        })))?.unwrap_id();
+
+    let candidate2020 = market.do_request(Request::Create(
+        Item::Pred(Pred {
+            pred_name: String::from("Candidate wins 2020 election"),
+            pred_args: ArgList::from("person"),
+            pred_value: None
+        })))?.unwrap_id();
+
+    let party2020 = market.do_request(Request::Create(
+        Item::Pred(Pred {
+            pred_name: String::from("Party wins 2020 election"),
+            pred_args: ArgList::from("party"),
+            pred_value: None
+        })))?.unwrap_id();
+
+    market.do_request(Request::Create(
+        Item::Depend(Depend {
+            depend_type: String::from("requires"),
+            depend_pred1: candidate2020.clone(),
+            depend_pred2: nominee2020,
+            depend_vars: ArgList::from("x"),
+            depend_args1: ArgList::from("x"),
+            depend_args2: ArgList::from("x.party, x")
+        })))?;
+
+    market.do_request(Request::Create(
+        Item::Depend(Depend {
+            depend_type: String::from("implies"),
+            depend_pred1: candidate2020.clone(),
+            depend_pred2: party2020,
+            depend_vars: ArgList::from("x"),
+            depend_args1: ArgList::from("x"),
+            depend_args2: ArgList::from("x.party")
+        })))?;
+
+    market.do_request(Request::Create(
+        Item::Pred(Pred {
+            pred_name: String::from("Atmospheric CO2 levels pass 500ppm"),
+            pred_args: ArgList::from("time"),
+            pred_value: None
+        })))?;
+
+    let trump_elected = market.do_request(Request::Create(
+        Item::Cond(Cond {
+            cond_pred: candidate2020.clone(),
+            cond_args: vec![trump.clone()],
+        })))?.unwrap_id();
+
+    let offer_id = market.do_request(Request::Create(
+        Item::Offer(Offer {
+            offer_user: mrfoo.clone(),
+            offer_cond_id: trump_elected.clone(),
+            offer_cond_time: None,
+            offer_buy_price: Dollars::from_millibucks(340),
+            offer_sell_price: Dollars::from_millibucks(450),
+            offer_buy_quantity: 100,
+            offer_sell_quantity: 200
+        })))?.unwrap_id();
+
+    market.do_request(Request::Update {
+        id: offer_id,
+        item_update: ItemUpdate::Offer(OfferUpdate {
+            offer_buy_price: Dollars::from_millibucks(360),
+            offer_sell_price: Dollars::from_millibucks(430),
+            offer_buy_quantity: 150,
+            offer_sell_quantity: 180
+        })})?;
+
+    let iou_id = market.do_request(Request::Create(
+        Item::IOU(IOU {
+            iou_issuer: mrfoo.clone(),
+            iou_holder: mrbar.clone(),
+            iou_value: Dollars::from_millibucks(170),
+            iou_cond_id: Some(trump_elected),
+            iou_cond_flag: true,
+            iou_cond_time: None,
+            iou_split: None,
+            iou_void: false
+        })))?.unwrap_id();
+/*
+    market.do_request(Request::Update {
+        id: iou_id,
+        item_update: ItemUpdate::Void
+    })?;
+*/
+    let mut holders = HashMap::new();
+    holders.insert(mrfoo.clone(), Dollars::from_millibucks(120));
+    holders.insert(mrbar.clone(), Dollars::from_millibucks(50));
+    let transfer = Transfer { holders };
+
+    market.do_request(Request::Update {
+        id: iou_id,
+        item_update: ItemUpdate::Transfer(transfer)
+    })?;
+
+    Ok(())
+}
+
+fn status(config: &Config) -> Result<(), Error> {
+    let db = DB::open_read_only(&config.db_filename)?;
+    let mut market = Market::open_existing(db)?;
+    println!("{:?}", market.info);
+    market.do_request(Request::Query(Query::AllUser))?.print();
+    market.do_request(Request::Query(Query::AllIOU))?.print();
+    market.do_request(Request::Query(Query::AllCond))?.print();
+    market.do_request(Request::Query(Query::AllOffer))?.print();
+    market.do_request(Request::Query(Query::AllEntity))?.print();
+    market.do_request(Request::Query(Query::AllRel))?.print();
+    market.do_request(Request::Query(Query::AllPred))?.print();
+    market.do_request(Request::Query(Query::AllDepend))?.print();
+    Ok(())
 }
 
 impl Response {
@@ -98,186 +294,10 @@ impl Response {
             Response::Items(_) => panic!("expected ID!")
         }
     }
-}
 
-fn do_command(cmd: Command) -> Result<(), Error> {
-    match cmd {
-        Command::Init(file) => {
-            let db = DB::open_read_write(&file)?;
-            let mut market = Market::create_new(db)?;
-
-            let mrfoo = market.do_request(Request::Create(
-                Item::User(User {
-                    user_name: String::from("Mr Foo"),
-                    user_locked: false
-                })))?.unwrap_id();
-
-            let mrbar = market.do_request(Request::Create(
-                Item::User(User {
-                    user_name: String::from("Mr Bar"),
-                    user_locked: false
-                })))?.unwrap_id();
-
-            let trump = market.do_request(Request::Create(
-                Item::Entity(Entity {
-                    entity_name: String::from("Donald Trump"),
-                    entity_type: String::from("person"),
-                })))?.unwrap_id();
-
-            let jeb = market.do_request(Request::Create(
-                Item::Entity(Entity {
-                    entity_name: String::from("Jeb Bush"),
-                    entity_type: String::from("person"),
-                })))?.unwrap_id();
-
-            let repub = market.do_request(Request::Create(
-                Item::Entity(Entity {
-                    entity_name: String::from("Republican Party"),
-                    entity_type: String::from("party"),
-                })))?.unwrap_id();
-
-            let _dem = market.do_request(Request::Create(
-                Item::Entity(Entity {
-                    entity_name: String::from("Democratic Party"),
-                    entity_type: String::from("party"),
-                })))?.unwrap_id();
-
-            market.do_request(Request::Create(
-                Item::Rel(Rel {
-                    rel_type: String::from("party"),
-                    rel_from: jeb,
-                    rel_to: repub.clone(),
-                })))?;
-
-            market.do_request(Request::Create(
-                Item::Rel(Rel {
-                    rel_type: String::from("party"),
-                    rel_from: trump.clone(),
-                    rel_to: repub,
-                })))?;
-
-            let nominee2020 = market.do_request(Request::Create(
-                Item::Pred(Pred {
-                    pred_name: String::from("Party nominee for 2020 election"),
-                    pred_args: ArgList::from("party,person"),
-                    pred_value: None
-                })))?.unwrap_id();
-
-            let candidate2020 = market.do_request(Request::Create(
-                Item::Pred(Pred {
-                    pred_name: String::from("Candidate wins 2020 election"),
-                    pred_args: ArgList::from("person"),
-                    pred_value: None
-                })))?.unwrap_id();
-
-            let party2020 = market.do_request(Request::Create(
-                Item::Pred(Pred {
-                    pred_name: String::from("Party wins 2020 election"),
-                    pred_args: ArgList::from("party"),
-                    pred_value: None
-                })))?.unwrap_id();
-
-            market.do_request(Request::Create(
-                Item::Depend(Depend {
-                    depend_type: String::from("requires"),
-                    depend_pred1: candidate2020.clone(),
-                    depend_pred2: nominee2020,
-                    depend_vars: ArgList::from("x"),
-                    depend_args1: ArgList::from("x"),
-                    depend_args2: ArgList::from("x.party, x")
-                })))?;
-
-            market.do_request(Request::Create(
-                Item::Depend(Depend {
-                    depend_type: String::from("implies"),
-                    depend_pred1: candidate2020.clone(),
-                    depend_pred2: party2020,
-                    depend_vars: ArgList::from("x"),
-                    depend_args1: ArgList::from("x"),
-                    depend_args2: ArgList::from("x.party")
-                })))?;
-
-            market.do_request(Request::Create(
-                Item::Pred(Pred {
-                    pred_name: String::from("Atmospheric CO2 levels pass 500ppm"),
-                    pred_args: ArgList::from("time"),
-                    pred_value: None
-                })))?;
-            
-            let trump_elected = market.do_request(Request::Create(
-                Item::Cond(Cond {
-                    cond_pred: candidate2020.clone(),
-                    cond_args: vec![trump.clone()],
-                })))?.unwrap_id();
-
-            let offer_id = market.do_request(Request::Create(
-                Item::Offer(Offer {
-                    offer_user: mrfoo.clone(),
-                    offer_cond_id: trump_elected.clone(),
-                    offer_cond_time: None,
-                    offer_buy_price: Dollars::from_millibucks(340),
-                    offer_sell_price: Dollars::from_millibucks(450),
-                    offer_buy_quantity: 100,
-                    offer_sell_quantity: 200
-                })))?.unwrap_id();
-
-            market.do_request(Request::Update {
-                id: offer_id,
-                item_update: ItemUpdate::Offer(OfferUpdate {
-                    offer_buy_price: Dollars::from_millibucks(360),
-                    offer_sell_price: Dollars::from_millibucks(430),
-                    offer_buy_quantity: 150,
-                    offer_sell_quantity: 180
-                })})?;
-
-            let iou_id = market.do_request(Request::Create(
-                Item::IOU(IOU {
-                    iou_issuer: mrfoo.clone(),
-                    iou_holder: mrbar.clone(),
-                    iou_value: Dollars::from_millibucks(170),
-                    iou_cond_id: Some(trump_elected),
-                    iou_cond_flag: true,
-                    iou_cond_time: None,
-                    iou_split: None,
-                    iou_void: false
-                })))?.unwrap_id();
-/*
-            market.do_request(Request::Update {
-                id: iou_id,
-                item_update: ItemUpdate::Void
-            })?;
-*/
-            let mut holders = HashMap::new();
-            holders.insert(mrfoo.clone(), Dollars::from_millibucks(120));
-            holders.insert(mrbar.clone(), Dollars::from_millibucks(50));
-            let transfer = Transfer { holders };
-
-            market.do_request(Request::Update {
-                id: iou_id,
-                item_update: ItemUpdate::Transfer(transfer)
-            })?;
-
-            Ok(())
-        }
-        Command::Status(file) => {
-            let db = DB::open_read_only(&file)?;
-            let mut market = Market::open_existing(db)?;
-            println!("{:?}", market.info);
-            print_response(&market.do_request(Request::Query(Query::AllUser))?);
-            print_response(&market.do_request(Request::Query(Query::AllIOU))?);
-            print_response(&market.do_request(Request::Query(Query::AllCond))?);
-            print_response(&market.do_request(Request::Query(Query::AllOffer))?);
-            print_response(&market.do_request(Request::Query(Query::AllEntity))?);
-            print_response(&market.do_request(Request::Query(Query::AllRel))?);
-            print_response(&market.do_request(Request::Query(Query::AllPred))?);
-            print_response(&market.do_request(Request::Query(Query::AllDepend))?);
-            Ok(())
-        }
+    fn print(&self) {
+        println!("{}", serde_json::to_string(self).unwrap())
     }
-}
-
-fn print_response(response: &Response) {
-    println!("{}", serde_json::to_string(response).unwrap())
 }
 
 // vi: ts=8 sts=4 et
---
+++ src/server.rs
@@ -0,0 +1,123 @@
+use std::str;
+use std::sync;
+use std::thread;
+use failure::{Error, err_msg};
+
+use futures;
+use futures::future::Future;
+use futures::sync::oneshot;
+use futures::stream::Stream;
+
+use hyper;
+use hyper::{Method, StatusCode};
+//use hyper::header::ContentLength;
+use hyper::server::{Http, Request, Response, Service};
+
+use serde_json;
+
+use market;
+use market::Market;
+
+struct Server {
+    channel: sync::mpsc::Sender<(market::msgs::Request, futures::sync::oneshot::Sender<market::msgs::Response>)>
+}
+
+#[derive(Debug)]
+enum AppError {
+    Canceled, // FIXME
+    Hyper(hyper::error::Error),
+    Json(serde_json::Error),
+    Utf8(str::Utf8Error),
+}
+
+impl Service for Server {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
+
+    fn call(&self, req: Request) -> Self::Future {
+
+        match (req.method(), req.path()) {
+            (&Method::Get, "/") => {
+                let mut response = Response::new();
+                response.set_body("Try POSTing JSON to /request");
+                Box::new(futures::future::ok(response))
+            },
+            (&Method::Post, "/request") => {
+                let tx = self.channel.clone();
+                Box::new(req.body().concat2().map_err(|e| AppError::Hyper(e)).and_then(|b| {
+                    let req_str = match str::from_utf8(&b) {
+                        Ok(req_str) => req_str,
+                        Err(utf8_error) => return Err(AppError::Utf8(utf8_error)),
+                    };
+                    serde_json::from_str::<market::msgs::Request>(req_str)
+                        .map_err(|e| AppError::Json(e))
+                }).map(move |market_req| {
+                    let (reply, on_reply) = oneshot::channel::<market::msgs::Response>();
+                    let msg = (market_req, reply);
+                    futures::future::result(tx.send(msg))
+                        .map_err(|_| AppError::Canceled)
+                        .and_then(|_| {
+                            on_reply.map_err(|_| AppError::Canceled).and_then(|market_reply| {
+                                serde_json::to_string(&market_reply).map_err(|e| AppError::Json(e))
+                            })
+                        })
+                }).flatten().then(|res| {
+                    match res {
+                        Ok(reply_json) => Ok(Response::new().with_body(reply_json)),
+                        Err(err) => match err {
+                            AppError::Canceled => Ok(Response::new()
+                                        .with_status(hyper::StatusCode::InternalServerError)
+                                        .with_body("internal server error")),
+                            AppError::Hyper(err0) => Err(err0),
+                            AppError::Json(err0) => Ok(Response::new()
+                                        .with_status(hyper::StatusCode::BadRequest)
+                                        .with_body(format!("{:?}", err0))),
+                            AppError::Utf8(err0) => Ok(Response::new()
+                                        .with_status(hyper::StatusCode::BadRequest)
+                                        .with_body(format!("{:?}", err0))),
+                        },
+                    }
+                }))
+            },
+            _ => {
+                let mut response = Response::new();
+                response.set_status(StatusCode::NotFound);
+                Box::new(futures::future::ok(response))
+            }
+        }
+    }
+}
+
+fn work_thread(
+    mut market: Market,
+    rx: sync::mpsc::Receiver<(market::msgs::Request, futures::sync::oneshot::Sender<market::msgs::Response>)>,
+) -> Result<(), Error> {
+    loop {
+        let (req, reply) = rx.recv()?;
+        let response = market.do_request(req)?;
+        match reply.send(response) {
+            Ok(()) => {},
+            Err(_) => break,
+        }
+    }
+    Ok(())
+}
+
+pub fn run_server(market: Market, addr_str: &str) -> Result<(), Error> {
+    let (tx, rx) = sync::mpsc::channel();
+    let thread_handle = thread::spawn(move || { work_thread(market, rx) });
+    let (_shutdown, on_shutdown) = oneshot::channel::<()>();
+    let addr = addr_str.parse()?;
+    Http::new().bind(&addr, move || Ok(Server { channel: tx.clone() }))?.run_until(on_shutdown.map_err(|_| ()))?;
+
+    match thread_handle.join() {
+        Ok(res) => res,
+        Err(_) => Err(err_msg("could not join thread")),
+    }
+
+    // FIXME shutdown server
+}
+
+// vi: ts=8 sts=4 et
